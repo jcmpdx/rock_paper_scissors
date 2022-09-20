a6221887f836e65005f4037a778879746fe6a6af
JCWin <jcm91186@gmail.com>
2022-09-19 19:45:42 -0700

    Created gameRound() in app.js

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
-const array = ['Rock', 'Paper', 'Scissors'];
+const array = ['rock', 'paper', 'scissors'];
+let playerSelection = '';
+let computerSelection = '';
 
 function getComputerChoice() {
-    let comptuterChoice = array[Math.floor(Math.random() * array.length)];
-    console.log(comptuterChoice);
+    computerSelection = array[Math.floor(Math.random() * array.length)];
+    return computerSelection;
 }
+
+let gameRound = () => {
+    let playerChoice= prompt("Rock Paper or Scissors?",'');
+    playerSelection = playerChoice.toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log('player selected: ' + playerSelection);
+    console.log('computer selected: ' + computerSelection);
+    if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+        alert(`Invalid input. Type rock, paper, or scissors as an input.`)
+    }
+    while (computerSelection == 'rock') {
+        (playerSelection == 'paper') ? console.log(`You win! ${playerSelection} beats ${computerSelection}`) : playerSelection == computerSelection ? console.log(`Tie! Both selected ${playerSelection}`) : console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        break;
+    }
+    while (computerSelection == 'paper') {
+        (playerSelection == 'scissors') ? console.log(`You win! ${playerSelection} beats ${computerSelection}`) : playerSelection == computerSelection ? console.log(`Tie! Both selected ${playerSelection}`) : console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        break;
+    }
+    while (computerSelection == 'scissors') {
+        (playerSelection == 'rock') ? console.log(`You win! ${playerSelection} beats ${computerSelection}`) : playerSelection == computerSelection ? console.log(`Tie! Both selected ${playerSelection}`) : console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        break;
+    }
+};
+
+gameRound();
