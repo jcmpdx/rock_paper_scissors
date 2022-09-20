3c8a96af399bc895d5ba2d60f1807a6746bb7729
JCWin <jcm91186@gmail.com>
2022-09-20 12:44:15 -0700

    Recreated function to play game, now called playRound(). Added <p> to html

--- app.js
+++ app.js
@@ -1,33 +1,67 @@
 const array = ['rock', 'paper', 'scissors'];
-let playerSelection = '';
-let computerSelection = '';
 
 function getComputerChoice() {
     computerSelection = array[Math.floor(Math.random() * array.length)];
     return computerSelection;
 }
 
-let gameRound = () => {
-    let playerChoice= prompt("Rock Paper or Scissors?",'');
+function getPlayerChoice() {
+    playerChoice = prompt("Rock Paper or Scissors?",'');
     playerSelection = playerChoice.toLowerCase();
+    if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+        alert(`Invalid input. Type rock, paper, or scissors as an input.`);
+        return playerSelection = null;
+    }
+    return playerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = getPlayerChoice();
     computerSelection = getComputerChoice();
     console.log('player selected: ' + playerSelection);
     console.log('computer selected: ' + computerSelection);
-    if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-        alert(`Invalid input. Type rock, paper, or scissors as an input.`)
-    }
     while (computerSelection == 'rock') {
-        (playerSelection == 'paper') ? console.log(`You win! ${playerSelection} beats ${computerSelection}`) : playerSelection == computerSelection ? console.log(`Tie! Both selected ${playerSelection}`) : console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        if (playerSelection == 'paper') {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            winCount += 1;
+        } else if (playerSelection == computerSelection) {
+            console.log(`Tie! Both selected ${playerSelection}`);
+        } else {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            loseCount += 1;
+        }
         break;
     }
     while (computerSelection == 'paper') {
-        (playerSelection == 'scissors') ? console.log(`You win! ${playerSelection} beats ${computerSelection}`) : playerSelection == computerSelection ? console.log(`Tie! Both selected ${playerSelection}`) : console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        if (playerSelection == 'scissors') {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            winCount += 1;
+        } else if (playerSelection == computerSelection) {
+            console.log(`Tie! Both selected ${playerSelection}`);
+        } else {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            loseCount += 1; 
+        }
         break;
     }
     while (computerSelection == 'scissors') {
-        (playerSelection == 'rock') ? console.log(`You win! ${playerSelection} beats ${computerSelection}`) : playerSelection == computerSelection ? console.log(`Tie! Both selected ${playerSelection}`) : console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        if (playerSelection == 'rock') {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            winCount += 1;
+        } else if (playerSelection == computerSelection) {
+            console.log(`Tie! Both selected ${playerSelection}`);
+        } else {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            loseCount += 1; 
+        }
         break;
     }
-};
+}
 
-gameRound();
+
+let playerSelection = '';
+let computerSelection = '';
+let winCount = 0;
+let loseCount = 0;
+playRound();
+
